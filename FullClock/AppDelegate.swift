ba371c052035c586185be50619dd32fd508ca852
sasama <sasama@sv11-kb.eecs.tottori-u.ac.jp>
2015-10-01 18:20:26 +0900

    Initial Commit

---
+++ FullClock/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FullClock
+//
+//  Created by sasama on 2015/10/01.
+//  Copyright (c) 2015年 sasama. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FullClock/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FullClock
+//
+//  Created by sasama on 2015/10/01.
+//  Copyright (c) 2015年 sasama. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
